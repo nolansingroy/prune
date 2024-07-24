6b825b31b885535d0e7dd17b6917db4d1e9fcbbe
Nolan <nolansingroy@gmail.com>
2024-07-24 14:49:24 -0500

    update useFetchEvents

--- src/hooks/useFetchEvents.ts
+++ src/hooks/useFetchEvents.ts
@@ -1,9 +1,7 @@
-// hooks/useFetchEvents.ts
-
 import { useState, useEffect } from "react";
 import { auth, db } from "../../firebase";
 import { collection, query, getDocs } from "firebase/firestore";
-import type { EventInput } from "@fullcalendar/core"; // or from "@fullcalendar/react"
+import { EventInput } from "../pages/types"; // Ensure correct import path
 
 const useFetchEvents = () => {
   const [events, setEvents] = useState<EventInput[]>([]);
@@ -13,21 +11,23 @@ const useFetchEvents = () => {
     const fetchEvents = async () => {
       const user = auth.currentUser;
       if (user) {
+        console.log("Fetching events...");
         const q = query(collection(db, "users", user.uid, "events"));
         const querySnapshot = await getDocs(q);
         const eventsData = querySnapshot.docs.map((doc) => {
           const data = doc.data();
           return {
             id: doc.id,
-            title: data.title,
+            title: data.title || "No title", // Ensure a title is always present
             start: data.start.toDate(), // Convert Firestore Timestamp to Date
-            end: data.end.toDate(), // Convert Firestore Timestamp to Date
-            description: data.description,
-            display: data.isBackgroundEvent ? "background" : "auto",
-            className: data.isBackgroundEvent ? "fc-bg-event" : "",
+            end: data.end?.toDate() || data.start.toDate(), // Provide a default or use the start date
+            description: data.description || "", // Include description or a default
+            display: data.isBackgroundEvent ? "background" : "auto", // Display as background if specified
+            isBackgroundEvent: !!data.isBackgroundEvent, // Ensure this is a boolean
+            className: data.isBackgroundEvent ? "fc-bg-event" : "", // Additional class based on condition
           };
         });
-        setEvents(eventsData);
+        setEvents(eventsData); // Set the events
       }
       setLoading(false);
     };
