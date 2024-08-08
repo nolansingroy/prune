3e89c8c29601f50e41754c1e37d4f07c92eb1273
Nolan <nolansingroy@gmail.com>
2024-08-08 15:05:02 -0500

    delete onBackground Events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,4 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export {onCreateFunction} from "./firestore/signUps";
-export {onBackgroundEvents} from "./firestore/backgroundEvents";
+// export {onBackgroundEvents} from "./firestore/backgroundEvents";
