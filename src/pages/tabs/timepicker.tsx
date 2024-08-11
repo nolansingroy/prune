6a0e7cf228c5a75b70a927a13a30d7547a5b314d
Nolan <nolansingroy@gmail.com>
2024-08-11 09:06:56 -0500

    update future time picker

--- src/pages/tabs/timepicker.tsx
+++ src/pages/tabs/timepicker.tsx
@@ -22,13 +22,13 @@ const MyTimePicker: React.FC<MyTimePickerProps> = ({ value, onChange }) => {
         locale="en-US"
         className="w-full text-lg p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
         // Add this inline styling to target shadow DOM elements if necessary
-        style={{
-          fontSize: "1rem",
-          height: "2.5rem",
-          width: "100%",
-          padding: "0.5rem",
-          boxSizing: "border-box",
-        }}
+        // style={{
+        //   fontSize: "1rem",
+        //   height: "2.5rem",
+        //   width: "100%",
+        //   padding: "0.5rem",
+        //   boxSizing: "border-box",
+        // }}
       />
     </div>
   );
