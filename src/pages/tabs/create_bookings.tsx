d37d192bc65ef2b5bacec3a9b85bc770fd310515
Nolan <nolansingroy@gmail.com>
2024-08-26 11:27:48 -0500

    Add create Booking page

--- src/pages/tabs/create_bookings.tsx
+++ src/pages/tabs/create_bookings.tsx
@@ -1,35 +1,20 @@
-"use client";
-
 import * as React from "react";
+import { useEffect, useState } from "react";
 import {
-  CaretSortIcon,
-  ChevronDownIcon,
-  DotsHorizontalIcon,
-} from "@radix-ui/react-icons";
-import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  writeBatch,
+  updateDoc,
+  Timestamp,
+  arrayUnion,
+} from "firebase/firestore";
+import { auth, db } from "../../../firebase";
+import { EventInput } from "../../interfaces/types";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import {
   Table,
@@ -39,275 +24,291 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { CaretSortIcon, PlusCircledIcon } from "@radix-ui/react-icons";
+// import { useState, useEffect } from "react";
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-  },
-];
-
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
-
-export const columns: ColumnDef<Payment>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    ),
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+type SortableKeys = "start" | "end" | "title";
 
 const CreateBookings: React.FC = () => {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
+  const [events, setEvents] = useState<EventInput[]>([]);
+  const [selectedRows, setSelectedRows] = useState<Set<string>>(new Set());
+  const [search, setSearch] = useState<string>("");
+  const [sortConfig, setSortConfig] = useState<{
+    key: SortableKeys;
+    direction: "asc" | "desc";
+  }>({ key: "start", direction: "asc" });
+
+  // Pagination states
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(5); // Adjust page size as needed
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      if (auth.currentUser) {
+        const eventsRef = collection(
+          db,
+          "users",
+          auth.currentUser.uid,
+          "events"
+        );
+        const q = query(eventsRef, where("isBackgroundEvent", "==", false));
+        const querySnapshot = await getDocs(q);
+        let expandedEvents: EventInput[] = [];
+
+        querySnapshot.docs.forEach((doc) => {
+          const data = doc.data();
+          const start =
+            data.start instanceof Timestamp
+              ? data.start.toDate()
+              : new Date(data.start);
+          const end =
+            data.end instanceof Timestamp
+              ? data.end.toDate()
+              : new Date(data.end);
+
+          expandedEvents.push({
+            id: doc.id,
+            title: data.title,
+            start: start,
+            end: end,
+            description: data.description || "",
+            display: data.display,
+            className: data.className,
+            isBackgroundEvent: data.isBackgroundEvent,
+            startDate: start,
+            startDay: start.toLocaleDateString("en-US", {
+              weekday: "long",
+              timeZone: "UTC",
+            }),
+            endDate: end,
+            endDay: end.toLocaleDateString("en-US", {
+              weekday: "long",
+              timeZone: "UTC",
+            }),
+          });
+        });
+
+        setEvents(expandedEvents);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const handleSort = (key: SortableKeys) => {
+    let direction: "asc" | "desc" = "asc";
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
+      direction = "desc";
+    }
+    setSortConfig({ key, direction });
+    setEvents((prevEvents: any) =>
+      [...prevEvents].sort((a, b) => {
+        if (a[key] < b[key]) return direction === "asc" ? -1 : 1;
+        if (a[key] > b[key]) return direction === "asc" ? 1 : -1;
+        return 0;
+      })
+    );
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredEvents = events.filter(
+    (event) =>
+      event.title.toLowerCase().includes(search.toLowerCase()) ||
+      (event.description ?? "").toLowerCase().includes(search.toLowerCase())
   );
 
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
+  // Pagination logic
+  const paginatedEvents = filteredEvents.slice(
+    pageIndex * pageSize,
+    (pageIndex + 1) * pageSize
+  );
 
-  const [rowSelection, setRowSelection] = React.useState({});
+  const handlePreviousPage = () => {
+    setPageIndex((old) => Math.max(old - 1, 0));
+  };
 
-  const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  });
+  const handleNextPage = () => {
+    setPageIndex((old) =>
+      Math.min(old + 1, Math.ceil(filteredEvents.length / pageSize) - 1)
+    );
+  };
+
+  const toggleRowSelection = (id: string) => {
+    const newSelectedRows = new Set(selectedRows);
+    if (newSelectedRows.has(id)) {
+      newSelectedRows.delete(id);
+    } else {
+      newSelectedRows.add(id);
+    }
+    setSelectedRows(newSelectedRows);
+  };
+
+  const deleteSelectedEvents = async () => {
+    if (
+      window.confirm("Are you sure you want to delete the selected events?")
+    ) {
+      const batch = writeBatch(db);
+      selectedRows.forEach((id) => {
+        const docRef = doc(
+          db,
+          "users",
+          auth.currentUser?.uid ?? "",
+          "events",
+          id
+        );
+        batch.delete(docRef);
+      });
+
+      await batch.commit();
+      setEvents(
+        events.filter((event) => event.id && !selectedRows.has(event.id))
+      );
+      setSelectedRows(new Set());
+    }
+  };
+
+  const totalEvents = filteredEvents.length;
+  const startItemIndex = pageIndex * pageSize + 1;
+  const endItemIndex = Math.min(startItemIndex + pageSize - 1, totalEvents);
 
   return (
-    <div className="w-full">
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
+    <div className="w-full relative">
+      <h1 className="text-xl font-bold mb-4">Create Bookings</h1>
+      <Button onClick={deleteSelectedEvents} disabled={selectedRows.size === 0}>
+        Delete Selected
+      </Button>
+      <Input
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="Search by title or description"
+        className="mb-4"
+      />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              <Checkbox
+                checked={selectedRows.size === paginatedEvents.length}
+                onCheckedChange={(checked) => {
+                  if (checked) {
+                    const newSelectedRows = new Set<string>();
+                    paginatedEvents.forEach((event) => {
+                      if (event.id) newSelectedRows.add(event.id);
+                    });
+                    setSelectedRows(newSelectedRows);
+                  } else {
+                    setSelectedRows(new Set());
+                  }
+                }}
+              />
+            </TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Day</TableHead>
+            <TableHead>
+              <div className="flex items-center">
+                Start Time
+                <button onClick={() => handleSort("start")}>
+                  <CaretSortIcon />
+                </button>
+              </div>
+            </TableHead>
+            <TableHead>
+              <div className="flex items-center">
+                End Time
+                <button onClick={() => handleSort("end")}>
+                  <CaretSortIcon />
+                </button>
+              </div>
+            </TableHead>
+            <TableHead>
+              <div className="flex items-center">
+                Title
+                <button onClick={() => handleSort("title")}>
+                  <CaretSortIcon />
+                </button>
+              </div>
+            </TableHead>
+            <TableHead>Notes</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {paginatedEvents.map((event) => (
+            <TableRow key={event.id || ""}>
+              <TableCell>
+                <Checkbox
+                  checked={selectedRows.has(event.id || "")}
+                  onCheckedChange={() => {
+                    if (event.id) {
+                      toggleRowSelection(event.id);
                     }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
+                  }}
+                />
+              </TableCell>
+              <TableCell>
+                {event.startDate instanceof Date &&
+                !isNaN(event.startDate.getTime())
+                  ? event.startDate.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    })
+                  : "Invalid Date"}
+              </TableCell>
+              <TableCell>{event.startDay}</TableCell>
+              <TableCell>
+                {event.start instanceof Date && !isNaN(event.start.getTime())
+                  ? event.start.toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })
+                  : "Invalid Date"}
+              </TableCell>
+              <TableCell>
+                {event.end instanceof Date && !isNaN(event.end.getTime())
+                  ? event.end.toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })
+                  : "Invalid Date"}
+              </TableCell>
+              <TableCell>
+                {event.title || (
+                  <span className="text-gray-500">Enter title</span>
+                )}
+              </TableCell>
+              <TableCell>
+                {event.description || (
+                  <span className="text-gray-500">Enter description</span>
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      {/* Row Selection and Pagination Controls */}
+      <div className="flex justify-between mt-4">
+        <span>{`${selectedRows.size} of ${filteredEvents.length} row(s) selected`}</span>
+
+        {/* Pagination Controls */}
+        <div className="flex items-center  -ml-24">
+          <span className="mx-2">
+            Showing {startItemIndex}-{endItemIndex} of {totalEvents}
+          </span>
+          <Button onClick={handlePreviousPage} disabled={pageIndex === 0}>
             Previous
           </Button>
+
           <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
+            onClick={handleNextPage}
+            disabled={
+              pageIndex === Math.ceil(filteredEvents.length / pageSize) - 1
+            }
           >
             Next
           </Button>
