93961f4338fb648abdcf93ea23ec17308d9ee9e9
Nolan <nolansingroy@gmail.com>
2024-09-24 11:18:38 -0700

    add comment

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -602,7 +602,7 @@ export default function Calendar() {
   if (eventsLoading) {
     return <div>Loading...</div>;
   }
-
+  // test
   const checkOverlap = (
     event: {
       extendedProps: { isBackgroundEvent: any };
