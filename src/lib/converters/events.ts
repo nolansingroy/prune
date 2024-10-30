d383ac00a0dba664eb07f0051724f08c8d38ebb3
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-10-30 07:51:37 -0700

    fix:fixing availabilities listview

--- src/lib/converters/events.ts
+++ src/lib/converters/events.ts
@@ -268,3 +268,81 @@ export const fetchBookingsListviewEvents = async (
   });
   return eventsList;
 };
+
+// Function to fetch availabilities from today onwards
+export const fetchAvailabilitiesListviewEvents = async (
+  userId: string
+): Promise<Omit<EventInput, "fee">[]> => {
+  if (!userId) {
+    console.warn("No user ID provided.");
+    return [];
+  }
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set time to midnight to match the start of the day
+
+  // Fetch events by "start" field where the date is from today onwards
+  const q = query(
+    eventRef(userId),
+    where("isBackgroundEvent", "==", true),
+    where("start", ">=", today), // Fetch events starting from today onwards
+    orderBy("start", "asc") // Ascending order
+  );
+
+  const querySnapshot = await getDocs(q);
+  let eventsList: Omit<EventInput, "fee">[] = [];
+
+  querySnapshot.docs.forEach((doc) => {
+    const data = doc.data();
+    console.log("Event data:", data);
+    const start =
+      data.start instanceof Timestamp
+        ? data.start.toDate()
+        : new Date(data.start);
+    const end =
+      data.end instanceof Timestamp ? data.end.toDate() : new Date(data.end);
+
+    if (data.recurrence) {
+      const dtstart = new Date(start);
+      eventsList.push({
+        id: doc.id,
+        title: data.title,
+        type: data.type || "",
+        typeId: data.typeId || "",
+        clientId: data.clientId || "",
+        clientName: data.clientName || "",
+        start: dtstart,
+        end: new Date(dtstart.getTime() + (end.getTime() - start.getTime())), // Calculate end time based on duration
+        description: data.description || "",
+        isBackgroundEvent: data.isBackgroundEvent,
+        startDate: dtstart,
+        startDay: dtstart.toLocaleDateString("en-US", { weekday: "long" }),
+        endDate: new Date(
+          dtstart.getTime() + (end.getTime() - start.getTime())
+        ),
+        endDay: new Date(
+          dtstart.getTime() + (end.getTime() - start.getTime())
+        ).toLocaleDateString("en-US", { weekday: "long" }),
+        recurrence: data.recurrence,
+        exceptions: data.exceptions,
+      });
+    } else {
+      eventsList.push({
+        id: doc.id,
+        title: data.title,
+        type: data.type || "",
+        typeId: data.typeId || "",
+        clientId: data.clientId || "",
+        clientName: data.clientName || "",
+        start: start,
+        end: end,
+        description: data.description || "",
+        isBackgroundEvent: data.isBackgroundEvent,
+        startDate: start,
+        startDay: start.toLocaleDateString("en-US", { weekday: "long" }),
+        endDate: end,
+        endDay: end.toLocaleDateString("en-US", { weekday: "long" }),
+      });
+    }
+  });
+  return eventsList;
+};
