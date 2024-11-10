b763d2e76ba044c6831caf8c854d3b8c6501d4ba
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-11-10 12:57:16 -0800

    fix:removed the text above login form and signup form

--- src/app/(auth)/_components/sigin-view.tsx
+++ src/app/(auth)/_components/sigin-view.tsx
@@ -49,14 +49,14 @@ export default function SignInViewPage() {
             ></Image>
           </div>
 
-          <div className="flex flex-col space-y-2 text-center">
+          {/* <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Sign in to your account
             </h1>
             <p className="text-sm text-muted-foreground">
               Enter your email below to sign in to your account
             </p>
-          </div>
+          </div> */}
           <LoginForm />
           {/* <p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
--- src/app/(auth)/_components/signup-view.tsx
+++ src/app/(auth)/_components/signup-view.tsx
@@ -62,14 +62,14 @@ export default function SignUpViewPage() {
               height={51}
             ></Image>
           </div>
-          <div className="flex flex-col space-y-2 text-center">
+          {/* <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Create an account
             </h1>
             <p className="text-sm text-muted-foreground">
               Enter your email below to create your account
             </p>
-          </div>
+          </div> */}
           <SignupForm />
           {/* <p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
