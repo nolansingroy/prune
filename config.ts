624360fa4cd64f6c4d95f0f8d188a94144a2da34
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-11-14 06:07:19 -0800

    fix:editing sessions cookies expiring in 5 days

--- config.ts
+++ config.ts
@@ -9,7 +9,7 @@ export const serverConfig = {
     httpOnly: true,
     secure: process.env.USE_SECURE_COOKIES === "true",
     sameSite: "lax" as const,
-    maxAge: 12 * 60 * 60 * 24,
+    maxAge: 5 * 24 * 60 * 60,
   },
   serviceAccount: {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
