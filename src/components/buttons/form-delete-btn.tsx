e728bf89c1d92bdbe8dd7b20d28c47e346c3ca6f
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-11-26 06:44:25 -0800

    fix:adding form delete button

---
+++ src/components/buttons/form-delete-btn.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { useFormStatus } from "react-dom";
+import { cn } from "@/lib/utils";
+
+type FormButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+};
+
+export default function FormDeleteButton({
+  children,
+  className,
+  onClick,
+}: FormButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      onClick={onClick}
+      disabled={pending}
+      variant="destructive"
+      // size="sm"
+      className={cn("", className)}
+    >
+      {children}
+    </Button>
+  );
+}
