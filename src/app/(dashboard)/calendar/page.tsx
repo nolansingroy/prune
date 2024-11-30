5301dc9d34d6e552152f5b28b9485135d13774c4
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-11-30 11:54:33 -0800

    fix:fixing login and signup and logout

--- src/app/(dashboard)/calendar/page.tsx
+++ src/app/(dashboard)/calendar/page.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import PageContainer from "@/components/layout/page-container";
 import FullCalendarComponent from "./_components/full-calendar";
-import { getDecodedToken } from "@/lib/helpers/tokens";
+import { cookies } from "next/headers";
+import { getTokens } from "next-firebase-auth-edge";
+import { notFound } from "next/navigation";
+import { fetchBookingsListviewEvents } from "@/lib/converters/events";
+import { clientConfig, serverConfig } from "../../../../config";
 
 export default async function CalendarPage() {
-  const tokens = await getDecodedToken();
+  const tokens = await getTokens(cookies(), {
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    serviceAccount: serverConfig.serviceAccount,
+  });
+
+  if (!tokens) {
+    notFound();
+  }
+
   console.log(tokens);
+
+  const result = await fetchBookingsListviewEvents(tokens.decodedToken.uid);
+  console.log(result);
 
   return <FullCalendarComponent />;
 }
