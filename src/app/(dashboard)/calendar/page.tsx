3ae47725b8776ab9eae0155d9122d6ef8ebc9161
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-12-01 07:58:37 -0800

    fix:removing server code

--- src/app/(dashboard)/calendar/page.tsx
+++ src/app/(dashboard)/calendar/page.tsx
@@ -5,23 +5,29 @@ import { getTokens } from "next-firebase-auth-edge";
 import { notFound } from "next/navigation";
 import { fetchBookingsListviewEvents } from "@/lib/converters/events";
 import { authConfig } from "../../../../config/server-config";
+import { getFirestore } from "firebase-admin/firestore";
+import { getFirebaseAdminApp } from "../../../../firebase-admin";
 
+// const db = getFirestore(getFirebaseAdminApp());
 export default async function CalendarPage() {
-  const tokens = await getTokens(cookies(), {
-    apiKey: authConfig.apiKey,
-    cookieName: authConfig.cookieName,
-    cookieSignatureKeys: authConfig.cookieSignatureKeys,
-    serviceAccount: authConfig.serviceAccount,
-  });
+  // const tokens = await getTokens(cookies(), {
+  //   apiKey: authConfig.apiKey,
+  //   cookieName: authConfig.cookieName,
+  //   cookieSignatureKeys: authConfig.cookieSignatureKeys,
+  //   serviceAccount: authConfig.serviceAccount,
+  // });
 
-  if (!tokens) {
-    notFound();
-  }
+  // if (!tokens) {
+  //   notFound();
+  // }
 
-  console.log(tokens);
+  // console.log(tokens);
 
-  // const result = await fetchBookingsListviewEvents(tokens.decodedToken.uid);
-  // console.log(result);
+  // const snapshot = await db
+  //   .collection("users")
+  //   .doc(tokens.decodedToken.uid)
+  //   .get();
+  // console.log(snapshot.data());
 
   return <FullCalendarComponent />;
 }
