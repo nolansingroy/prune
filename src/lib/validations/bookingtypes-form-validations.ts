006908c5b25a65fddc3c0e9ce5e77a1d47a40be0
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-12-02 12:35:27 -0800

    fix:fix invalid type issue

--- src/lib/validations/bookingtypes-form-validations.ts
+++ src/lib/validations/bookingtypes-form-validations.ts
@@ -4,12 +4,12 @@ export const bookingtypeFormSchema = z.object({
   name: z
     .string()
     .min(1, { message: "Name must be at least 1 character long" }),
-  fee: z.number().positive({ message: "Fee must be a positive number" }),
-  color: z
-    .string()
-    .regex(/^#([0-9A-F]{3}){1,2}$/i, {
-      message: "Color must be a valid hex value",
-    }),
+  fee: z
+    .number({ invalid_type_error: "Number required" })
+    .positive({ message: "Fee must be a positive number" }),
+  color: z.string().regex(/^#([0-9A-F]{3}){1,2}$/i, {
+    message: "Color must be a valid hex value",
+  }),
 });
 
 export type TBookingtypeForm = z.infer<typeof bookingtypeFormSchema>;
